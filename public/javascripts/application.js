6097e3c12ef0c7814559fea1d758aeb24d0bc383
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-05-05 18:17:10 -0700

    It seems canvas requires image objects.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@ var unicornTo = function(endingPoint) {
     var zoom = 13; // how to figure this out, dammit :D
     var url = 'http://maps.googleapis.com/maps/api/staticmap?center=San+Francisco,CA&zoom='+zoom+'&size=512x512&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Ccolor:red%7Clabel:C%7C40.718217,-73.998284&sensor=false&path='+startingPoint+'|' + decodeURIComponent(endingPoint);
 
-    return $('<img src="'+url+'" width="" height="">');
+    return new Image(url);
 };
 $(function() {
     /* jQuery stuff */
