0a8ff2e9d5899cd82edf070e9cccb2808a630ace
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-05-05 17:38:22 -0700

    Implemented a really easy way to include my face in the app.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,7 @@
 $(function() {
     /* jQuery stuff */
+    $('[data-twitter-account]').each(function() {
+        var img = $('<img src='+'https://api.twitter.com/1/users/profile_image?screen_name='+$(this).attr('data-twitter-account')+'&size=normal'+'>');
+        $(this).html(img);
+    });
 });
