40b2599befa4d2bbacbaabc42593f02020d7ebf3
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-05-05 20:33:43 -0700

    Get the ride

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@ var unicornTo = function(endingPoint) {
     var zoom = 13; // how to figure this out, dammit :D
     var url = 'http://maps.googleapis.com/maps/api/staticmap?center=San+Francisco,CA&zoom='+zoom+'&size=512x512&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Ccolor:red%7Clabel:C%7C40.718217,-73.998284&sensor=false&path='+startingPoint+'|' + decodeURIComponent(endingPoint);
 
-    return new Image(url);
+    return url;
 };
 $(function() {
     $('#unicornride').click(function() {
@@ -22,9 +22,4 @@ $(function() {
         });
     });
 
-    /* jQuery stuff */
-    $('[data-twitter-account]').each(function() {
-        var img = $('<img src='+'https://api.twitter.com/1/users/profile_image?screen_name='+$(this).attr('data-twitter-account')+'&size=normal'+'>');
-        $(this).html(img);
-    });
 });
