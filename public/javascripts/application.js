e46e148a8dcba6de237d57952d03260aa04715c4
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-05-05 18:12:01 -0700

    This function will take the unicorn to its goal.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,10 @@
+var unicornTo = function(endingPoint) {
+    var startingPoint = '169+11th+street,San+francisco,CA'; // Made for @BallmerPeak, DO NOT CHANGE! - Raimo
+    var zoom = 13; // how to figure this out, dammit :D
+    var url = 'http://maps.googleapis.com/maps/api/staticmap?center=San+Francisco,CA&zoom='+zoom+'&size=512x512&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Ccolor:red%7Clabel:C%7C40.718217,-73.998284&sensor=false&path='+startingPoint+'|' + decodeURIComponent(endingPoint);
+
+    return $('<img src="'+url+'" width="" height="">');
+};
 $(function() {
     /* jQuery stuff */
     $('[data-twitter-account]').each(function() {
